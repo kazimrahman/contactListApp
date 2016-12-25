7fb391c76921b211bb0ec10809589d13f48c3262
Kazi Rahman <kazirahman@kazis-mbp.attlocal.net>
2016-12-24 22:24:40 -0800

    add is fully working

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -7,13 +7,23 @@ function($scope, $http){
    //   console.log("i got the data i requested");
    //   $scope.contactlist = response;
    //});
-     $http.get('/contactlist').then(function(response){
+
+   var refresh = function(){
+      $http.get('/contactlist').then(function(response){
         console.log("i got the data i requested: " + response.data);
         $scope.contactlist = response.data;
-    });﻿
+        $scope.contact = null;
+      });﻿
+    };
+
+    refresh();
 
     $scope.addContact = function(){
       console.log($scope.contact);
+      $http.post('/contactlist', $scope.contact).then(function(response){
+         console.log(response);
+         refresh();
+      });
     };
 
 //   $scope.contactlist = contactlist;
--- server.js
+++ server.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
 var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
 app.get('/contactlist', function(req, res){
    console.log("received get");
@@ -14,6 +16,14 @@ app.get('/contactlist', function(req, res){
       res.json(docs);
    });
 });
+
+app.post('/contactlist', function(req, res) {
+   console.log(req.body);
+   db.contactlist.insert(req.body, function(err, doc){
+      res.json(doc);
+   });
+});
+
 
 app.listen(3000);
 console.log("Server running on port 3000");
